fa3abf04d1a3695bf59fb8ba0d64716d71069a91
Christopher Caroni <christophercaroni97@gmail.com>
2021-04-15 20:33:25 +0200

    missing brace

--- plugins/PromisedScene/main.ts
+++ plugins/PromisedScene/main.ts
@@ -40,14 +40,13 @@ module.exports = async (ctx: MyContext): Promise<SceneOutput> => {
   // Checking all of the arguments are set in the plugin
 
   const envApiKey = process.env.TPDB_API_KEY;
-
-  $logger.verbose(`env has TPDB API key: ${!!envApiKey}`)
   if (!Object.hasOwnProperty.call(args, "apiKey")) {
     if (envApiKey) {
       args.apiKey = process.env.TPDB_API_KEY;
     } else {
       $throw("Missing apiKey in plugin args!");
     }
+  }
 
   if (!Object.hasOwnProperty.call(args, "useTitleInSearch")) {
     $logger.warn("Missing useTitleInSearch in plugin args!");
