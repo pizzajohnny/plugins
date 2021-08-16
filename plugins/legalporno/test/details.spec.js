d878b5508acf6a4e23a7b568cbbe8c84d61cb661
boi12321 <46821810+boi123212321@users.noreply.github.com>
2021-08-16 19:11:54 +0200

    fix LP tests

--- plugins/legalporno/test/details.spec.js
+++ plugins/legalporno/test/details.spec.js
@@ -23,10 +23,10 @@ describe("Legalporno", () => {
     expect(result.actors).to.deep.equal(["Ornella Morgan"]);
     expect(result.labels).to.deep.equal([
       "3+ on 1",
-      "Czech",
       "anal",
       "big butt",
       "cum swallowing",
+      "czech",
       "double anal (DAP)",
       "facial cumshot",
       "first time",
@@ -59,7 +59,7 @@ describe("Legalporno", () => {
     ]);
     expect(result.labels).to.deep.equal([
       "3+ on 1",
-      "American",
+      "american",
       "anal",
       "average anal gape",
       "blowjob",
